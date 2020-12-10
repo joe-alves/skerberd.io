60d7563b06d8d108192f7425ce539ef7fa10c197
Joe Alves <jmalves@appacademy.io>
2020-12-10 12:18:46 -0800

    Empty name form validation

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -6,13 +6,20 @@ const Form = () => {
   
   const appObject = useContext(AppContext);
   const [possibleNewPlayerName, setName] = useState("Joe");
+  const [nameEmptyError, setNameEmptyError] = useState(false);
   return (
     <form id="add-new-player-form" onSubmit={e => {
         e.preventDefault();
-        appObject.addNewPlayer(possibleNewPlayerName);
+        setNameEmptyError(false);
+        if (possibleNewPlayerName.trim().length === 0) {
+          setNameEmptyError(true);
+          return;
+        }
+        appObject.addNewPlayer(possibleNewPlayerName.trim());
         setName("");
     }}>
       <h3>Add a New Player</h3>
+      {nameEmptyError && <span className="error-message">Please enter a name.</span>}
       <input
         type="text"
         placeholder="Player Name"
